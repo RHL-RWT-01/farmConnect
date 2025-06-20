7b6f476f6eb784e8d58fe8616feda4fcbc68b506
Rahul Rawat <rahulrawat4060@gmail.com>
2025-06-20 14:05:36 +0530

    ! using nextauth fo now

--- components/providers/sessionProvider.tsx
+++ components/providers/sessionProvider.tsx
@@ -1,8 +1,12 @@
-"use client"
+// "use client"
 
-import { SessionProvider } from "next-auth/react"
-import { ReactNode } from "react"
+// import { SessionProvider } from "next-auth/react"
+// import { ReactNode } from "react"
 
-export default function AuthProvider({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
-}
+// export default function AuthProvider({ children }: { children: ReactNode }) {
+//   return <SessionProvider>{children}</SessionProvider>
+// }
+
+
+
+            // for now not using next-auth, but can be used later
